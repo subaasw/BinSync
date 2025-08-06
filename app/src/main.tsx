e1bf5161fe7377f180f87413beca68c532a052a0
Subash Giri <mrsbs.giri@gmail.com>
2025-08-07 00:43:18 +0545

    feat: backend functionality

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
+import { Toaster } from "@/components/ui/sonner"
 import RoutingPages from "./routes";
 import "./index.css";
 
@@ -8,6 +9,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <RoutingPages />
+      <Toaster />
     </BrowserRouter>
   </StrictMode>
 );
